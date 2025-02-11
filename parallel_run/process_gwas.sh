240cb610484c0c1eca5edced09355aa3384cca58
Kijoung Song <kys21207@gmail.com>
2025-02-11 15:46:25 -0500

    Update process_gwas.sh

--- parallel_run/process_gwas.sh
+++ parallel_run/process_gwas.sh
@@ -3,35 +3,30 @@
 # Input arguments
 cell_db=$1
 gwas_file=$2
-cell_covariance=$3
-#output_dir=$4
 
 # Extract filename without extension
 cell_name_prefix=$(basename "$cell_db" .db)
-gwas_name_prefix=$(basename "$gwas_file")
+gwas_name_prefix=$(basename "$gwas_file" .regenie*.gz)
 
-# Start time
-start_time=$(date +%s)
+# Check the number of columns in the GWAS file
+column_count=$(zcat ${gwas_file} | awk 'NR==1 {print NF}')
 
-# Run SPrediXcan
-python /app/MetaXcan/software/SPrediXcan.py \
-  --model_db_path ${cell_db} \
-  --covariance ${cell_covariance} \
-  --gwas_file ${gwas_file} \
-  --snp_column SNPID \
-  --effect_allele_column ALLELE1 \
-  --non_effect_allele_column ALLELE0 \
-  --beta_column BETA \
-  --se_column SE \
-  --zscore_column ZSCORE \
-  --keep_non_rsid \
-  --pvalue_column PVALUE \
-  --output_file ${gwas_name_prefix}.predixcan_${cell_name_prefix}.csv
+# Set the column indices based on the number of columns
+if [ "$column_count" -eq 14 ]; then
+  beta_col=10
+  se_col=11
+  log10p_col=13
+elif [ "$column_count" -eq 12 ]; then
+  beta_col=8
+  se_col=9
+  log10p_col=11
+else
+  echo "Unexpected number of columns ($column_count) in ${gwas_file}"
+  exit 1
+fi
 
-# Upload result to DNAnexus
-#dx upload ${output_dir}/${gwas_name_prefix}.predixcan_${cell_name_prefix}.csv --destination ${output_dir}/
+# Create input file for SPrediXcan
+echo -e "rsid\tCHROM\tALLELE0\tALLELE1\tBETA\tZ\tP" > ${gwas_name_prefix}.input.txt
+zcat ${gwas_file} | awk -v beta_col=$beta_col -v se_col=$se_col -v log10p_col=$log10p_col 'NR>1 {rsid="chr"$1"_"$2"_"$4"_"$5"_b38"; Z=$beta_col/$se_col; print rsid"\t"$1"\t"$4"\t"$5"\t"$beta_col"\t"Z"\t"10^(-$log10p_col)}' >> ${gwas_name_prefix}.input.txt
+gzip ${gwas_name_prefix}.input.txt
 
-# End time
-end_time=$(date +%s)
-runtime=$((end_time - start_time))
-echo "Processing time for ${gwas_name_prefix}.predixcan_${cell_name_prefix}: ${runtime} seconds"
