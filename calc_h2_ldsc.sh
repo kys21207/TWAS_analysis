b79470b36caaebc43e7bc5a732b3838456e970d8
Kijoung Song <kys21207@gmail.com>
2025-02-13 14:13:58 -0500

    Create calc_h2_ldsc.sh

---
+++ calc_h2_ldsc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the file paths
+gwas_file="/mnt/project/genuity_data/tmp_gwas_data/dx_uc_v3.input.txt.gz"
+index_file="/opt/notebooks/convert_spredixcanID_to_ldscID_data.txt.gz"
+output_file="/opt/notebooks/dx_uc_v3.txt.gz"
+
+# Perform the merge operation directly on gzipped files
+(
+    # Print header
+    echo -e "CHROM\tALLELE0\tALLELE1\tBETA\tZ\tP\tdbSNP_rsid"
+    
+    # Join and format the data
+    zcat "$index_file" | \
+    awk 'NR==FNR{a[$1]=$2; next} $1 in a{print $2, $3, $4, $5, $6, $7, a[$1]}' OFS="\t" - \
+    <(zcat "$gwas_file") 
+) | gzip > "$output_file"
+
+
+./munge_sumstats.py \
+--out /opt/notebooks/dx_uc_v3 \
+--p P \
+--a1 ALLELE1 \
+--a2 ALLELE0 \
+--signed-sumstats Z,0 \
+--N 14012 \
+--snp dbSNP_rsid \
+--sumstats ${output_file} 
+
+cp -r /mnt/project/publically_available_supporting_files/GRCh38/ /opt/notebooks/
+tar -xvzf /opt/notebooks/GRCh38/baselineLD_v2.2.tgz
+tar -xvzf /opt/notebooks/GRCh38/weights.tgz 
+
+ldsc.py \
+--h2 /opt/notebooks/dx_uc_v3.sumstats.gz \
+--ref-ld-chr baselineLD_v2.2/baselineLD.@ \
+--w-ld-chr weights/weights.hm3_noMHC.@ \
+--out /opt/notebooks/dx_uc_v3
